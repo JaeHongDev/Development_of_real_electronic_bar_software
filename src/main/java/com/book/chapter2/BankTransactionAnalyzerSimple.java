8cc8c2e4480dd9324c53cd6b416066b63522d281
jaehong <println@kakao.com>
2022-08-09 14:20:42 +0900

    챕터2장

---
+++ src/main/java/com/book/chapter2/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,26 @@
+package com.book.chapter2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    private static final String RESOURCES = "src/main/resoureces/";
+
+    public static void main(final String... args)  throws IOException{
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES  + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLineFrom(lines);
+
+        System.out.println("The total for all transactions is ");
+    }
+}
